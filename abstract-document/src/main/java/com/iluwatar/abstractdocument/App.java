735d4818a9237b8d47c7ce0c0296320e606554cf
chenlong <chenlong88882001@163.com>
2016-06-14 14:18:36 +0800

    修改 java-design-patterns 代码

--- abstract-document/src/main/java/com/iluwatar/abstractdocument/App.java
+++ abstract-document/src/main/java/com/iluwatar/abstractdocument/App.java
@@ -44,45 +44,47 @@ import java.util.Map;
  */
 public class App {
 
-  /**
-   * Executes the App
-   */
-  public App() {
-    System.out.println("Constructing parts and car");
+	/**
+	 * Executes the App
+	 */
+	public App() {
 
-    Map<String, Object> carProperties = new HashMap<>();
-    carProperties.put(HasModel.PROPERTY, "300SL");
-    carProperties.put(HasPrice.PROPERTY, 10000L);
+		System.out.println( "Constructing parts and car" );
 
-    Map<String, Object> wheelProperties = new HashMap<>();
-    wheelProperties.put(HasType.PROPERTY, "wheel");
-    wheelProperties.put(HasModel.PROPERTY, "15C");
-    wheelProperties.put(HasPrice.PROPERTY, 100L);
+		Map< String, Object > carProperties = new HashMap<>();
+		carProperties.put( HasModel.PROPERTY, "300SL" );
+		carProperties.put( HasPrice.PROPERTY, 10000L );
 
-    Map<String, Object> doorProperties = new HashMap<>();
-    doorProperties.put(HasType.PROPERTY, "door");
-    doorProperties.put(HasModel.PROPERTY, "Lambo");
-    doorProperties.put(HasPrice.PROPERTY, 300L);
+		Map< String, Object > wheelProperties = new HashMap<>();
+		wheelProperties.put( HasType.PROPERTY, "wheel" );
+		wheelProperties.put( HasModel.PROPERTY, "15C" );
+		wheelProperties.put( HasPrice.PROPERTY, 100L );
 
-    carProperties.put(HasParts.PROPERTY, Arrays.asList(wheelProperties, doorProperties));
+		Map< String, Object > doorProperties = new HashMap<>();
+		doorProperties.put( HasType.PROPERTY, "door" );
+		doorProperties.put( HasModel.PROPERTY, "Lambo" );
+		doorProperties.put( HasPrice.PROPERTY, 300L );
 
-    Car car = new Car(carProperties);
+		carProperties.put( HasParts.PROPERTY, Arrays.asList( wheelProperties, doorProperties ) );
 
-    System.out.println("Here is our car:");
-    System.out.println("-> model: " + car.getModel().get());
-    System.out.println("-> price: " + car.getPrice().get());
-    System.out.println("-> parts: ");
-    car.getParts().forEach(p -> System.out
-        .println("\t" + p.getType().get() + "/" + p.getModel().get() + "/" + p.getPrice().get()));
-  }
+		Car car = new Car( carProperties );
 
-  /**
-   * Program entry point
-   *
-   * @param args command line args
-   */
-  public static void main(String[] args) {
-    new App();
-  }
+		System.out.println( "Here is our car:" );
+		System.out.println( "-> model: " + car.getModel().get() );
+		System.out.println( "-> price: " + car.getPrice().get() );
+		System.out.println( "-> parts: " );
+		car.getParts().forEach( p -> System.out
+				.println( "\t" + p.getType().get() + "/" + p.getModel().get() + "/" + p.getPrice().get() ) );
+	}
+
+	/**
+	 * Program entry point
+	 *
+	 * @param args command line args
+	 */
+	public static void main( String[] args ) {
+
+		new App();
+	}
 
 }
