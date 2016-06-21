4f74ad52769a9457b9b1a8cbc0b49b81540249a2
chenlong <chenlong88882001@163.com>
2016-06-21 21:45:41 +0800

    修改 java-design-patterns  建造者模式 代码

--- builder/src/main/java/com/iluwatar/builder/Hero.java
+++ builder/src/main/java/com/iluwatar/builder/Hero.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright (c) 2014 Ilkka Seppälä
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,127 +23,140 @@
 package com.iluwatar.builder;
 
 /**
- * 
+ *
  * Hero, the class with many parameters.
- * 
+ *
  */
 public final class Hero {
 
-  private final Profession profession;
-  private final String name;
-  private final HairType hairType;
-  private final HairColor hairColor;
-  private final Armor armor;
-  private final Weapon weapon;
+	private final Profession profession;
+	private final String     name;
+	private final HairType   hairType;
+	private final HairColor  hairColor;
+	private final Armor      armor;
+	private final Weapon     weapon;
 
-  private Hero(Builder builder) {
-    this.profession = builder.profession;
-    this.name = builder.name;
-    this.hairColor = builder.hairColor;
-    this.hairType = builder.hairType;
-    this.weapon = builder.weapon;
-    this.armor = builder.armor;
-  }
+	private Hero( Builder builder ) {
 
-  public Profession getProfession() {
-    return profession;
-  }
+		this.profession = builder.profession;
+		this.name = builder.name;
+		this.hairColor = builder.hairColor;
+		this.hairType = builder.hairType;
+		this.weapon = builder.weapon;
+		this.armor = builder.armor;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public Profession getProfession() {
 
-  public HairType getHairType() {
-    return hairType;
-  }
+		return profession;
+	}
 
-  public HairColor getHairColor() {
-    return hairColor;
-  }
+	public String getName() {
 
-  public Armor getArmor() {
-    return armor;
-  }
+		return name;
+	}
 
-  public Weapon getWeapon() {
-    return weapon;
-  }
+	public HairType getHairType() {
 
-  @Override
-  public String toString() {
+		return hairType;
+	}
 
-    StringBuilder sb = new StringBuilder();
-    sb.append("This is a ")
-            .append(profession)
-            .append(" named ")
-            .append(name);
-    if (hairColor != null || hairType != null) {
-      sb.append(" with ");
-      if (hairColor != null) {
-        sb.append(hairColor).append(' ');
-      }
-      if (hairType != null) {
-        sb.append(hairType).append(' ');
-      }
-      sb.append(hairType != HairType.BALD ? "hair" : "head");
-    }
-    if (armor != null) {
-      sb.append(" wearing ").append(armor);
-    }
-    if (weapon != null) {
-      sb.append(" and wielding a ").append(weapon);
-    }
-    sb.append('.');
-    return sb.toString();
-  }
+	public HairColor getHairColor() {
 
-  /**
-   * 
-   * The builder class.
-   * 
-   */
-  public static class Builder {
+		return hairColor;
+	}
 
-    private final Profession profession;
-    private final String name;
-    private HairType hairType;
-    private HairColor hairColor;
-    private Armor armor;
-    private Weapon weapon;
+	public Armor getArmor() {
 
-    /**
-     * Constructor
-     */
-    public Builder(Profession profession, String name) {
-      if (profession == null || name == null) {
-        throw new IllegalArgumentException("profession and name can not be null");
-      }
-      this.profession = profession;
-      this.name = name;
-    }
+		return armor;
+	}
 
-    public Builder withHairType(HairType hairType) {
-      this.hairType = hairType;
-      return this;
-    }
+	public Weapon getWeapon() {
 
-    public Builder withHairColor(HairColor hairColor) {
-      this.hairColor = hairColor;
-      return this;
-    }
+		return weapon;
+	}
 
-    public Builder withArmor(Armor armor) {
-      this.armor = armor;
-      return this;
-    }
+	@Override
+	public String toString() {
 
-    public Builder withWeapon(Weapon weapon) {
-      this.weapon = weapon;
-      return this;
-    }
+		StringBuilder sb = new StringBuilder();
+		sb.append( "This is a " )
+				.append( profession )
+				.append( " named " )
+				.append( name );
+		if ( hairColor != null || hairType != null ) {
+			sb.append( " with " );
+			if ( hairColor != null ) {
+				sb.append( hairColor ).append( ' ' );
+			}
+			if ( hairType != null ) {
+				sb.append( hairType ).append( ' ' );
+			}
+			sb.append( hairType != HairType.BALD ? "hair" : "head" );
+		}
+		if ( armor != null ) {
+			sb.append( " wearing " ).append( armor );
+		}
+		if ( weapon != null ) {
+			sb.append( " and wielding a " ).append( weapon );
+		}
+		sb.append( '.' );
+		return sb.toString();
+	}
 
-    public Hero build() {
-      return new Hero(this);
-    }
-  }
+	/**
+	 *
+	 * The builder class.
+	 *
+	 */
+	public static class Builder {
+
+		private final Profession profession;
+		private final String     name;
+		private       HairType   hairType;
+		private       HairColor  hairColor;
+		private       Armor      armor;
+		private       Weapon     weapon;
+
+		/**
+		 * Constructor
+		 */
+		public Builder( Profession profession, String name ) {
+
+			if ( profession == null || name == null ) {
+				throw new IllegalArgumentException( "profession and name can not be null" );
+			}
+			this.profession = profession;
+			this.name = name;
+		}
+
+		public Builder withHairType( HairType hairType ) {
+
+			this.hairType = hairType;
+			return this;
+		}
+
+		public Builder withHairColor( HairColor hairColor ) {
+
+			this.hairColor = hairColor;
+			return this;
+		}
+
+		public Builder withArmor( Armor armor ) {
+
+			this.armor = armor;
+			return this;
+		}
+
+		public Builder withWeapon( Weapon weapon ) {
+
+			this.weapon = weapon;
+			return this;
+		}
+
+		public Hero build() {
+
+			return new Hero( this );
+		}
+	}
 }
